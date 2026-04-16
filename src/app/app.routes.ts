dfc47defa45ae134b897f2e45615ad1b3157f2ad
TeRaccoon <jack.sam.benning@gmail.com>
2026-04-16 21:43:07 +0100

    Configured routes for parent dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,35 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './auth/auth-guard';
-import { ChoreDashboard } from './components/chore-dashboard/chore-dashboard';
-import { ChoreDetails } from './components/chore-details/chore-details';
-import { CreateAccount } from './components/create-account/create-account';
-import { Login } from './components/login/login';
+import { CreateAccount } from './components/auth/create-account/create-account';
+import { Login } from './components/auth/login/login';
+import { ChoreDetails } from './components/chore/chore-details/chore-details';
+import { ChoreSettings } from './components/parent/chore-settings/chore-settings';
+import { DashboardHome } from './components/parent/dashboard-home/dashboard-home';
+import { DashboardLayout } from './components/parent/dashboard-layout/dashboard-layout';
+import { KidsSettings } from './components/parent/kids-settings/kids-settings';
+import { ParentSettings } from './components/parent/parent-settings/parent-settings';
+import { CreateProfile } from './components/start/children/create-profile/create-profile';
 import { Start } from './components/start/start';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Start, canActivate: [authGuard] },
-  { path: 'dashboard', component: ChoreDashboard, canActivate: [authGuard] },
+  {
+    path: 'dashboard',
+    component: DashboardLayout,
+    canActivate: [authGuard],
+    children: [
+      { path: 'home', component: DashboardHome, pathMatch: 'full' },
+      { path: 'chores', component: ChoreSettings },
+      {
+        path: 'kids',
+        component: KidsSettings,
+      },
+      { path: 'kids/add', component: CreateProfile },
+      { path: 'settings', component: ParentSettings },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
   { path: 'chore/:id', component: ChoreDetails, canActivate: [authGuard] },
   { path: 'login', component: Login },
   { path: 'create-account', component: CreateAccount },
